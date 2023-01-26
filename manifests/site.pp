4308d6bfd46d6f667123c6f28855da1347c821bb
Sebek <sebekforgit@gmail.com>
2023-01-26 12:15:36 +0100

    Changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,11 @@
+# node default {
+#   file { '/root/README':
+#     ensure => file,
+#     content => 'To jest pliczek czytajmie\n',
+#     owner   => 'root',
+#   }
+# }
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'To jest pliczek czytajmie\n',
-    owner   => 'root',
-  }
-}
-node 'master.puppet.vm' {
   include role::master_server
 }
 node /^web/ { 
