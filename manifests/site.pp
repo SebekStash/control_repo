3c2e0008f9af48afa1c133137c3dd740371cbf4a
Sebek <sebekforgit@gmail.com>
2023-01-26 12:20:44 +0100

    Changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,22 @@
-# node default {
-#   file { '/root/README':
-#     ensure => file,
-#     content => 'To jest pliczek czytajmie\n',
-#     owner   => 'root',
-#   }
-# }
 node default {
-  include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => 'To jest pliczek czytajmie\n',
+    owner   => 'root',
+  }
+  file {'/root/DodatkowyPlik':
+    ensure => file
+    content => "plik nr2"
+    owner => 'root'
+    mode => '400'
+  }
 }
-node /^web/ { 
-  include role::app_server
-}
-node /^db/ {
-  include role::db_server
-}
+# node default {
+#   include role::master_server
+# }
+# node /^web/ { 
+#   include role::app_server
+# }
+# node /^db/ {
+#   include role::db_server
+# }
