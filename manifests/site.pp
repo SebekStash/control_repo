5a99e4ef6dc2f996dac2eca8ac64be783b6b6e75
Sebek <sebekforgit@gmail.com>
2023-01-26 12:59:35 +0100

    Changes nr.5

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,8 +4,18 @@ node default {
     content => 'This is a readme',
     owner   => 'root',
   }
-  include role::master_server
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
+}
+node /^web/ { 
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
 }
