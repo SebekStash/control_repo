7a8cbf9e1087782c710dd8f3691fa53a55a4e64a
Sebek <sebekforgit@gmail.com>
2023-02-03 11:18:45 +0100

    site.pp changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
-# node default {
-#   file { '/root/README':
-#     ensure => file,
-#     content => 'This is a readme',
-#     owner   => 'root',
-#   }
-# }
 node default {
-  include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
+# node default {
+#   include role::master_server
+# }
